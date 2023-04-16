a49f62174408615a0b255335ae0b8b91bd1ecec6
Swaroop Deshpande <swaroop.deshpande.20210101030@siics.org>
2023-04-16 11:56:17 +0530

    Added new files
    Signed-off-by: Swaroop Deshpande <swaroop.deshpande.20210101030@siics.org>

---
+++ Java/Multi4.java
@@ -0,0 +1,29 @@
+class Marvellous implements Runnable 
+{
+    public void run()
+    {
+        try{
+        for(int i=1; i<10; i++)
+        {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        }
+        catch(Exception obj)
+        {}
+    }
+}
+class Multi4
+{
+    public static void main(String a[])
+    {
+        Marvellous mobj1= new Marvellous();
+        Marvellous mobj2= new Marvellous();
+
+        Thread t1= new Thread(mobj1);
+        Thread t2= new Thread(mobj2);
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ Java/Multi7.java
@@ -0,0 +1,26 @@
+class Marvellous implements Runnable 
+{
+    public void run()
+    {
+        Display();
+    }
+    synchronized public void Display()
+    {
+        for(int i=1; i<10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
+class Multi7
+{
+    public static void main(String a[])
+    {
+        Marvellous mobj1= new Marvellous();
+        Marvellous mobj2= new Marvellous();
+        Thread t1= new Thread(mobj1);
+        Thread t2= new Thread(mobj2);
+        t1.start();
+        t2.start();
+    }
+}
